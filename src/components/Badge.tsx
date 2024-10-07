25dae4f08cd713349a973f33c578f776086e0a50
youriwatson <youri.wtsn@gmail.com>
2024-10-07 22:00:30 +0200

    "Goedemorgen Youri"

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,7 +48,7 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Goedemorgen Youri'}
+          {props.footer?.text ?? 'Powered by'}
           <a
             ref={liteBadge}
             href={props.footer?.companyLink ?? 'https://flowiseai.com'}
